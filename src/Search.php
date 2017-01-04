61ba309a0b74842e194ace663fac0bd887c36796
Daniel <dupshaw@ameronix.com>
2017-01-04 14:55:04 -0500

    Minor update

--- src/Search.php
+++ src/Search.php
@@ -90,7 +90,7 @@ class Search {
             'select' => '',
             'find' => '',
             'searchGroup' => '',
-            'from' => null,
+            'from' => '',
         ];
     }
 
