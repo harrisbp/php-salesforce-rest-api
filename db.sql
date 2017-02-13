fbbe9de463f3df5e69c612a5f063bf234dbcc4bb
Daniel <dupshaw@ameronix.com>
2017-02-13 13:44:25 -0500

    data column needs to be of type LONGTEXT because of describe content length

--- db.sql
+++ db.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS `salesforce_cache` (
 	`id` int(10) unsigned NOT NULL AUTO_INCREMENT,
 	`sfid` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
 	`type` varchar(60) COLLATE utf8mb4_unicode_ci NOT NULL,
-	`data` text COLLATE utf8mb4_unicode_ci NOT NULL,
+	`data` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
 	`sf_created_at` datetime NULL,
 	`sf_updated_at` datetime NULL,
 	`created_at` datetime NOT NULL,
